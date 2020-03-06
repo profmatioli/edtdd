9a6b76942dec672bb906394b07c783c144d7ca94
Prof. Matioli <matioli@cotil.unicamp.br>
2020-03-05 21:44:59 -0300

    Acrescentado cabecalhos das funções para a lista de exercicios propostaDD

--- funcoes.cpp
+++ funcoes.cpp
@@ -1,5 +1,46 @@
 #include "funcoes.h"
 
+//imolementação da função ex01
+
+//imolementação da função ex02
+
+//imolementação da função ex03
+
+//imolementação da função ex04
+
+//imolementação da função ex05
+
+//imolementação da função ex06
+
+//imolementação da função ex07
+
+//imolementação da função ex08
+
+//imolementação da função ex09
+
+//imolementação da função ex10
+
+//imolementação da função ex11
+
+//imolementação da função ex12
+
+//imolementação da função ex13
+
+//imolementação da função ex14
+
+//imolementação da função ex15
+
+//imolementação da função ex16
+
+//imolementação da função ex17
+
+//imolementação da função ex18
+
+//imolementação da função ex19
+
+
+
+//**********************************************************************
 int somaInt(int a, int b){
     return a+b;
 }
